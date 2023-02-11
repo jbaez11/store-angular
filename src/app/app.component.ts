12ed4fbd7b53df3cb6aa2cc1c555a82f3dfdf393
Jonathan Baez <jbaez9910@gmail.com>
2023-02-11 14:33:49 -0500

    conceptos iniciales de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product} from './product.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'store-angular';
+  name = 'jonathan';
+  age = 23;
+  img ='https://cataas.com/cat'
+  btnDisabled = true;
+  names:string[]=['j','n','v','t']
+  newName:string = '';
+  register={
+    name:'',
+    email:'',
+    password:''
+  }
+
+  products:Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/coca.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/coca.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/coca.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/coca.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/coca.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/coca.jpg'
+    }
+  ]
+
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge(){
+    this.age += 1;
+  }
+
+  onScroll(event : Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  addName(){
+    this.names.push(this.newName);
+  }
+  deleteB(index:number){
+  this.names.splice(index,1)
+  }
+  onRegister(){
+    console.log(this.register);
+  }
 }
